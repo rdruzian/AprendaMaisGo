8f453be53698d5c7aea393640b9760879440ca26
Renato Druzian <rendruzian@gmail.com>
2022-01-16 17:50:07 -0300

    feature/cadastro: Criado endpoint para cadastro do usuario

--- User/api/apiUser.go
+++ User/api/apiUser.go
@@ -1,18 +1,34 @@
 package apiUser
 
 import (
+	"../../User"
+	"../../database"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	//"../../database"
-	//"../../User"
-	//"../../Aluno"
-	//"../../Professor"
 )
 
 func Cadastro(c *gin.Context) {
-	//db := database.GetDatabase()
-	//
-	//var user User.Usuario
-	//var aluno Aluno.Aluno
-	//var prof Professor.Professor
+	db := database.GetDatabase()
+
+	var user User.Usuario
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Não foi possível fazer o bind do JSON: " + err.Error(),
+		})
+		return
+	}
+
+	result := db.Create(&user).Error
+	if result != nil {
+		c.JSON(400, gin.H{
+			"error": fmt.Sprintf("Não foi possível salvar o cadastrar o usuário: %v", result),
+		})
+	}
+
+	c.JSON(200, gin.H{
+		"menssagem": "Usuário cadastro com sucesso!",
+	})
 }
 
